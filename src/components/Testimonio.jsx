4148ee32c7a6ae111222ee001f69b3b29aea714a
emapeire <emapeire@outlook.com>
2022-05-26 18:41:59 -0300

    componentes

--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 import "../stylesheets/Testimonio.css";
 
+function Testimonio(props) {
+  return (
+    <div className="contenedor-testimonio">
+      <img 
+        className="imagen-testimonio" 
+        src={require(`../images/testimonio-${props.imagen}.png`)}
+        alt={`foto-${props.alt}`} />
+      <div className="contenido-testimonio">
+        <p className="nombre-testimonio"><strong>{props.nombre}</strong> en {props.pais}</p>
+        <p className="cargo-testimonio">{props.cargo} en <strong>{props.empresa}</strong></p>
+        <p className="texto-testimonio">"{props.testimonio}"</p>
+      </div>
+    </div>
+  );
+}
+
+/*
 class Testimonio extends React.Component {
   render() {
     return (
@@ -18,5 +35,6 @@ class Testimonio extends React.Component {
     );
   }
 }
+*/
 
 export default Testimonio;
